dbf292c5a5cbdf1928e7611f30b648328f3ed0c8
T.K. Kwon <tkkwon1998@gmail.com>
2023-06-01 16:49:13 -0400

    create multicall package with ability to send calls to functions with arguments

---
+++ multicall/viewcall.go
@@ -0,0 +1,156 @@
+package multicall
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"reflect"
+	"regexp"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+type ViewCall struct {
+	method    string
+	arguments []interface{}
+}
+
+type ViewCalls []ViewCall
+
+var insideParens = regexp.MustCompile("\\(.*?\\)")
+var numericArg = regexp.MustCompile("u?int(256)|(8)")
+
+func NewViewCall(method string, arguments []interface{}) ViewCall {
+	return ViewCall{
+		method:    method,
+		arguments: arguments,
+	}
+
+}
+
+func (call ViewCall) Validate() error {
+	if _, err := call.argsCallData(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (call ViewCall) argumentTypes() []string {
+	rawArgs := insideParens.FindAllString(call.method, -1)[0]
+	rawArgs = strings.Replace(rawArgs, "(", "", -1)
+	rawArgs = strings.Replace(rawArgs, ")", "", -1)
+	if rawArgs == "" {
+		return []string{}
+	}
+	args := strings.Split(rawArgs, ",")
+	for index, arg := range args {
+		args[index] = strings.Trim(arg, " ")
+	}
+	return args
+}
+
+func (call ViewCall) CallData() ([]byte, error) {
+	argsSuffix, err := call.argsCallData()
+	if err != nil {
+		return nil, err
+	}
+	methodPrefix, err := call.methodCallData()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := make([]byte, 0)
+	payload = append(payload, methodPrefix...)
+	payload = append(payload, argsSuffix...)
+
+	return payload, nil
+}
+
+func (call ViewCall) methodCallData() ([]byte, error) {
+	methodParts := strings.Split(call.method, ")(")
+	var method string
+	if len(methodParts) > 1 {
+		method = fmt.Sprintf("%s)", methodParts[0])
+	} else {
+		method = methodParts[0]
+	}
+	hash := crypto.Keccak256([]byte(method))
+	return hash[0:4], nil
+}
+
+func (call ViewCall) argsCallData() ([]byte, error) {
+	argTypes := call.argumentTypes()
+	if len(argTypes) != len(call.arguments) {
+		return nil, fmt.Errorf("number of argument types doesn't match with number of arguments with method %s", call.method)
+	}
+	argumentValues := make([]interface{}, len(call.arguments))
+	arguments := make(abi.Arguments, len(call.arguments))
+
+	for index, argTypeStr := range argTypes {
+		argType, err := abi.NewType(argTypeStr, "", nil)
+		if err != nil {
+			return nil, err
+		}
+
+		fmt.Println("arg type: ", argType)
+
+		arguments[index] = abi.Argument{Type: argType}
+		argumentValues[index], err = call.getArgument(index, argTypeStr)
+
+		fmt.Println("arg values: ", argumentValues[index])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return arguments.Pack(argumentValues...)
+}
+
+func (call ViewCall) getArgument(index int, argumentType string) (interface{}, error) {
+	arg := call.arguments[index]
+	if argumentType == "address" {
+		address, ok := arg.(string)
+		if !ok {
+			return nil, fmt.Errorf("expected address argument to be a string")
+		}
+		return toByteArray(address)
+	} else if numericArg.MatchString(argumentType) {
+		if num, ok := arg.(json.Number); ok {
+			if v, err := num.Int64(); err != nil {
+				return big.NewInt(v), nil
+			} else if v, err := num.Float64(); err != nil {
+				return big.NewInt(int64(v)), nil
+			} else {
+			}
+		} else {
+			int64 := reflect.TypeOf(int64(0))
+			argType := reflect.TypeOf(arg)
+			kind := argType.Kind()
+			if kind == reflect.String {
+				if val, ok := new(big.Int).SetString(call.arguments[index].(string), 10); !ok {
+					return nil, fmt.Errorf("could not parse %s as a base 10 number", call.arguments[index])
+				} else {
+					return val, nil
+				}
+			} else if argType.ConvertibleTo(int64) {
+				return big.NewInt(reflect.ValueOf(arg).Convert(int64).Int()), nil
+			}
+		}
+	}
+	return arg, nil
+}
+
+func toByteArray(address string) ([20]byte, error) {
+	var addressBytes [20]byte
+	address = strings.Replace(address, "0x", "", -1)
+	addressBytesSlice, err := hex.DecodeString(address)
+	if err != nil {
+		return addressBytes, err
+	}
+
+	copy(addressBytes[:], addressBytesSlice[:])
+	return addressBytes, nil
+}
--- query/query.go
+++ query/query.go
@@ -5,15 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/big"
 	"time"
 
 	"canto-api/config"
-	multicall "canto-api/contracts"
+	"canto-api/multicall"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/redis/go-redis/v9"
 )
@@ -24,6 +21,7 @@ type QueryEngine struct {
 	redisclient *redis.Client
 	ethclient   *ethclient.Client
 	interval    time.Duration
+	mcaddress   common.Address // multicall contract address
 }
 
 // Returns a QueryEngine instance with redis client, eth client, and interval.
@@ -32,81 +30,57 @@ func NewQueryEngine() *QueryEngine {
 		redisclient: config.RDB,
 		ethclient:   config.EthClient,
 		interval:    5,
+		mcaddress:   common.HexToAddress("0xcA11bde05977b3631167028862bE2a173976CA11"),
 	}
 }
 
 func (qe *QueryEngine) StartQueryEngine(ctx context.Context) {
 
-	// multicall contract address
-	mcaddress := common.HexToAddress("0xcA11bde05977b3631167028862bE2a173976CA11")
-	multicallInstance, err := multicall.NewMulticall(mcaddress, qe.ethclient)
+	multicallInstance, err := multicall.NewMulticall(qe.mcaddress, qe.ethclient)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// vc := multicall.NewViewCall(
+	// 	"getReserves()",
+	// 	[]interface{}{},
+	// )
+
+	vc := multicall.NewViewCall(
+		"balanceOf(address)(uint256)",
+		[]interface{}{"0x66945A3A0f7D3D85A5d1A309AA321436945A329A"},
+	)
+
+	payload, err := vc.CallData()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("total payload: ", payload)
+
 	// canto note pair address
-	pairAddress := common.HexToAddress("0x1D20635535307208919f0b67c3B2065965A85aA9")
+	// pairAddress := common.HexToAddress("0x1D20635535307208919f0b67c3B2065965A85aA9")
 
-	methodSuffix := "getReserves()"
-	hash := crypto.Keccak256([]byte(methodSuffix))
-	methodBytes := hash[:4]
-
-	payload := make([]byte, 0)
-	payload = append(payload, methodBytes...)
+	// usdc address
+	erc20Address := common.HexToAddress("0x80b5a32E4F032B2a058b4F29EC95EEfEEB87aDcd")
 
 	// multicall struct
 	reserves := multicall.Multicall3Call{
-		Target:   pairAddress,
+		Target:   erc20Address,
 		CallData: payload,
 	}
 
 	multicallArray := []multicall.Multicall3Call{reserves}
 
-	fmt.Println(multicallArray)
-
-	// pairInstance, err := multicall.NewPair(pairAddress, qe.ethclient)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	gasPrice, err := qe.ethclient.SuggestGasPrice(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	chainID := 7700
-	bigID := big.NewInt(int64(chainID))
-
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, bigID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	auth.Nonce = big.NewInt(int64(0))
-	auth.Value = big.NewInt(0)     // in wei
-	auth.GasLimit = uint64(300000) // in units
-	auth.GasPrice = gasPrice
-
 	ticker := time.NewTicker(qe.interval * time.Second)
 	for range ticker.C {
 		// call functions in multicall contract
-		fmt.Println("calling multicall contract")
 		res, err := multicallInstance.Aggregate(nil, multicallArray)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Println("called multicall contract")
-
-		// get reserves from pair contract
-		// reserves, err := pairInstance.GetReserves(nil)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
+		fmt.Println(res)
 
 		// must marshal reserves to json in order to store in redis
 		b, err := json.Marshal(res)
@@ -116,7 +90,7 @@ func (qe *QueryEngine) StartQueryEngine(ctx context.Context) {
 
 		// ret := abi.Unpack([]string{"uint112", "uint112", "uint32"}, res.ReturnData[0])
 
-		fmt.Println(b)
+		// fmt.Println(b)
 
 		// set key in redis
 		err = qe.redisclient.Set(ctx, "key", b, 0).Err()
