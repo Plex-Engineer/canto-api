0ae7af347bf4426e079a75882cbac03f9c121b5d
Sairam kola <sairamkola@Sairams-Laptop.local>
2023-06-14 12:33:02 +0530

    add contracts json and tests

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/redis/go-redis/v9"
@@ -21,6 +23,10 @@ func NewConfig() {
 		DB:       0,
 	})
 	EthClient, _ = ethclient.Dial("https://mainnode.plexnode.org:8545")
+	calls, err := getContractsFromJson("./config/jsons/contracts.json")
+	if err != nil {
+		fmt.Println("Error getting contracts from json:", err)
+	}
 	ContractCalls = calls
 	MulticallAddress = common.HexToAddress("0xcA11bde05977b3631167028862bE2a173976CA11")
 	QueryInterval = 5
