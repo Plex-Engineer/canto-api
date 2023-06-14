73d38e9c1134a2d5d1f8eb60f0a1933517374701
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-06-14 12:13:43 +0530

    modified the tokenkey

--- config/contracts.go
+++ config/contracts.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 )
 
@@ -23,7 +23,7 @@ func getContractsFromJsonFile() []Contract {
 	}
 
 	defer jsonFile.Close()
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := io.ReadAll(jsonFile)
 	// var result map[string]map[string]string interface{}
 	// json.Unmarshal([]byte(byteValue), &result)
 	res, err := UnmarshalTokens(byteValue)
@@ -34,15 +34,17 @@ func getContractsFromJsonFile() []Contract {
 
 	calls := []Contract{}
 
+	//get cTokens
 	for _, token := range res.CTokens {
+		tokenKey := token.Symbol
 		calls = append(calls, Contract{
 			Name:    token.Name,
 			Address: token.Address,
 			Keys: []string{
-				"cTokens:" + token.Address + ":getCash",
-				"cTokens:" + token.Address + ":exchangeRateStored",
-				"cTokens:" + token.Address + ":supplyRatePerBlock",
-				"cTokens:" + token.Address + ":borrowRatePerBlock",
+				"cTokens:" + tokenKey + ":getCash",
+				"cTokens:" + tokenKey + ":exchangeRateStored",
+				"cTokens:" + tokenKey + ":supplyRatePerBlock",
+				"cTokens:" + tokenKey + ":borrowRatePerBlock",
 			},
 			Methods: []string{
 				"getCash()(uint256)",
@@ -57,7 +59,62 @@ func getContractsFromJsonFile() []Contract {
 				{},
 			},
 		})
+
+		calls = append(calls, Contract{
+			Name:    "Router",
+			Address: "0xa252eEE9BDe830Ca4793F054B506587027825a8e",
+			Keys: []string{
+				"cTokens:" + tokenKey + ":price",
+			},
+			Methods: []string{
+				"getUnderlyingPrice(address)(uint256)",
+			},
+			Args: [][]interface{}{
+				{token.Address},
+			},
+		})
+
+		calls = append(calls, Contract{
+			Name:    "Comptroller",
+			Address: "0x5E23dC409Fc2F832f83CEc191E245A191a4bCc5C",
+			Keys: []string{
+				"cTokens:" + tokenKey + ":markets",
+				"cTokens:" + tokenKey + ":supplySpeeds",
+				"cTokens:" + tokenKey + ":borrowCaps",
+			},
+			Methods: []string{
+				"markets(address)(bool, uint256, bool)",
+				"compSupplySpeeds(address)(uint256)",
+				"borrowCaps(address)(uint256)",
+			},
+			Args: [][]interface{}{
+				{token.Address},
+				{token.Address},
+				{token.Address},
+			},
+		})
 	}
+
+	// //get tokens
+	// for _, token := range res.Tokens {
+	// 	tokenKey := token.Symbol
+	// 	calls = append(calls, Contract{
+	// 		Name:    token.Name,
+	// 		Address: token.Address,
+	// 		Keys: []string{
+	// 			"tokens:" + tokenKey + ":balanceOf",
+	// 			"tokens:" + tokenKey + ":totalSupply",
+	// 		},
+	// 		Methods: []string{
+	// 			"balanceOf(address)(uint256)",
+	// 			"totalSupply()(uint256)",
+	// 		},
+	// 		Args: [][]interface{}{
+	// 			{},
+	// 			{},
+	// 		},
+	// 	})
+	// }
 
 	return calls
 
