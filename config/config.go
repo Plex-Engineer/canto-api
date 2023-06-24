83b95da7cc270690afcad038a9e9aad55231c88e
T.K. Kwon <tkkwon1998@gmail.com>
2023-06-24 18:17:07 -0400

    remove keys.go in config

--- config/config.go
+++ config/config.go
@@ -9,6 +9,22 @@ import (
 	"google.golang.org/grpc"
 )
 
+const (
+	StakingAPR          = "STAKING_APR"
+	AllValidators       = "ALL_VALIDATORS"
+	ValidatorMap        = "VALIDATOR_MAP"
+	AllCSRs             = "ALL_CSRS"
+	CSRMap              = "CSR_MAP"
+	AllProposals        = "ALL_PROPOSALS"
+	ProposalMap         = "PROPOSAL_MAP"
+	Pairs               = "PAIRS"
+	ProcessedPairs      = "PROCESSED_PAIRS"
+	ProcessedPairsMap   = "PROCESSED_PAIRS_MAP"
+	CTokens             = "CTOKENS"
+	ProcessedCTokens    = "PROCESSED_CTokens"
+	ProcessedCTokensMap = "PROCESSED_CTokens_MAP"
+)
+
 var (
 	RDB              *redis.Client
 	EthClient        *ethclient.Client
